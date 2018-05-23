5b863ade7c1e7de2aa0c19310c8c3a6486c0d500
TheBeachmaster <otienoken@outlook.com>
2018-05-23 13:11:52 +0300

    Add CPP library sources

---
+++ solution/task1/cpp/lib/libclimate.cc
@@ -0,0 +1,49 @@
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+
+#include "libclimate.hpp"
+
+float Libclimate::gen_humidity(float dew_point, float temperature)
+{
+    float humidity = 100.00 * ((611 * exp(5423 * ((1 / 273)) - (1 / (dew_point + 273)))) / (611 * exp(5423 * ((1 / 273)-(1 / temperature)))));
+    return humidity;
+}
+
+float Libclimate::gen_pressure(float temperature)
+{
+    float pressure = 101325 * exp(((0.00 - 9.81) * 0.0289644 * (200))/(8.31444598 * (temperature + 273)));
+    return pressure;
+}
+
+float Libclimate::gen_wind_speed(float longitude)
+{
+    float wind_speed = ((0.0001) * longitude);
+    return wind_speed;
+}
+
+float Libclimate::dew_point_calculator(float relative_humidity)
+{
+    float dew_point = (100.00 - relative_humidity) / 5;
+    return dew_point;
+}
+
+float Libclimate::relative_humidity_calculator(void)
+{
+    srand((unsigned)time(0));
+    float upper_hum = 75.00;
+    float lower_hum = 55.00;
+    float humidity_range = (upper_hum - lower_hum) + 1;
+    float relative_humidity = lower_hum + (float)(humidity_range * rand() / (RAND_MAX + 1.00));
+    return relative_humidity;
+}
+
+float Libclimate::temperature_generator(void)
+{
+    srand((unsigned)time(0));
+    float upper_temp = 15.00;
+    float lower_temp = 42.00;
+    float temp_range = (upper_temp - lower_temp) + 1;
+    float temperature = lower_temp + (float)(temp_range * rand() / (RAND_MAX + 1.00));
+    return temperature;
+}
---
+++ solution/task1/cpp/lib/libclimate.hpp
@@ -0,0 +1,27 @@
+/**
+ * This is a simple library that generates Pressure, Humidity and Wind-speed data
+ * Given Barometric pressure in milliBars as : 101325 * exp(((0.00 - 9.81) * 0.0289644 * (200))/(8.31444598 * (temp + 273)))
+ * Given Winspeed for a given longitude as : (0.0001) * long
+ * Given Humdity as : 100.00 * ((611 * exp(5423 * ((1 / 273)) - (1 / (dp + 273)))) / (611 * exp(5423 * ((1 / 273)-(1 / temp)))))
+ */
+
+#if !defined(LIBCLIMATE_HPP)
+#define LIBCLIMATE_HPP
+
+class Libclimate
+{
+private:
+   float temperature;
+   float dew_point;
+   float longitude;
+public:
+    Libclimate() {}
+    float gen_pressure(float temperature);
+    float gen_wind_speed(float longitude);
+    float gen_humidity(float dew_point, float temperature);
+    float temperature_generator();
+    float dew_point_calculator(float relative_humidity);
+    float relative_humidity_calculator();
+};
+
+#endif // LIBCLIMATE_HPP
